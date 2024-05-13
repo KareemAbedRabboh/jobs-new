a419c17bd3c0c8931180a7598add2972f5b48a1b
Kareem <22210145@scme.edu.ps>
2024-05-13 20:46:47 +0300

    update 2

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © kja.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Design & Develop by <a href="https://www.instagram.com/k.ja18_/" class="text-decoration-underline">Kareem Abed Rabboh</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
